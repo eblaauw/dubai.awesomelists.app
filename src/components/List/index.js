869e96a474039104260b2448c7f4a93e92346b04
Edzo <edzo@edzo.dev>
2021-03-11 13:22:51 +0400

    Change wrong padding

--- src/components/List/index.js
+++ src/components/List/index.js
@@ -3,11 +3,11 @@ import PlacesJSON from './../../data/places.json';
 const List = () => (
     <div className="relative">
         <div className="smallShadowRight w-full">
-            <ul className="overflow-x-scroll overflow-y-hidden whitespace-no-wrap py-4">
+            <ul className="overflow-x-scroll overflow-y-hidden whitespace-no-wrap p-4">
                 {
                     Object.keys(PlacesJSON.categories).map((e, i) => {
                         return (
-                            <a href={'#' + PlacesJSON.categories[i].category_id} className="font-medium hover:font-bold p-4">
+                            <a href={'#' + PlacesJSON.categories[i].category_id} className="font-medium hover:font-bold py-4">
                                 <li key={i} className="inline-block bg-white mx-3 py-4 shadow-sm hover:shadow-md rounded-lg border-gray-100 border-2">
                                     <span type="img" className="text-center">{PlacesJSON.categories[i].category_emoji}</span>
                                 </li>
