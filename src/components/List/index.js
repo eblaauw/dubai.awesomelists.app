ff091cb9aac8d2ccd5b6328a5df72d63af850014
Edzo <edzo@edzo.dev>
2021-03-11 13:16:02 +0400

    Refactor menu list

--- src/components/List/index.js
+++ src/components/List/index.js
@@ -2,21 +2,21 @@ import React from "react"
 import PlacesJSON from './../../data/places.json';
 const List = () => (
     <div className="relative">
-    <div className="smallShadowRight w-full">
-        <ul className="overflow-x-scroll overflow-y-hidden whitespace-no-wrap">
-            {
-                Object.keys(PlacesJSON.categories).map((e, i) => {
-                    return (
-                        <li className="inline-block bg-white mx-3 p-4 shadow-sm hover:shadow-md rounded-lg border-gray-100	 border-2">
-                            <a href={'#' + PlacesJSON.categories[i].category_id} className="font-medium hover:font-bold">
-                                <span type="img" className="text-center">{PlacesJSON.categories[i].category_emoji}</span>
+        <div className="smallShadowRight w-full">
+            <ul className="overflow-x-scroll overflow-y-hidden whitespace-no-wrap py-4">
+                {
+                    Object.keys(PlacesJSON.categories).map((e, i) => {
+                        return (
+                            <a href={'#' + PlacesJSON.categories[i].category_id} className="font-medium hover:font-bold p-4">
+                                <li key={i} className="inline-block bg-white mx-3 p-4 shadow-sm hover:shadow-md rounded-lg border-gray-100	 border-2">
+                                    <span type="img" className="text-center">{PlacesJSON.categories[i].category_emoji}</span>
+                                </li>
                             </a>
-                        </li>
-                    )
-                })}
+                        )
+                    })}
 
-        </ul>
-    </div>
+            </ul>
+        </div>
     </div>
 )
 
