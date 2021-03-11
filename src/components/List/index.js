b648cd9f7b601a11c7131156b9ffb7a81523fc8a
Edzo <edzo@edzo.dev>
2021-03-11 14:49:06 +0400

    Fix scrolling in navbar

--- src/components/List/index.js
+++ src/components/List/index.js
@@ -3,12 +3,12 @@ import PlacesJSON from './../../data/places.json';
 const List = () => (
     <div className="relative">
         <div className="smallShadowRight w-full">
-            <ul className="overflow-x-scroll overflow-y-hidden whitespace-no-wrap p-4">
+            <ul className="overflow-x-scroll overflow-y-hidden whitespace-no-wrap py-4">
                 {
                     Object.keys(PlacesJSON.categories).map((e, i) => {
                         return (
                             <a href={'#' + PlacesJSON.categories[i].category_id} className="font-medium hover:font-bold py-4">
-                                <li key={i} className="inline-block bg-white mx-3 p-4 shadow-sm hover:shadow-md rounded-lg border-gray-100 border-2">
+                                <li key={i} className="inline-block bg-white mr-3 p-4 shadow-sm hover:shadow-md rounded-lg border-gray-100 border-2">
                                     <span type="img" className="text-center pr-2">{PlacesJSON.categories[i].category_emoji}</span>
                                     <span>{PlacesJSON.categories[i].category_name}</span>
                                 </li>
